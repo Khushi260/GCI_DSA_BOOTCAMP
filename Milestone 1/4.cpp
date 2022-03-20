67546ad007376b8b90fa81cc4722d3e4e02e3ddd
Khushi Jha <94845508+Khushi260@users.noreply.github.com>
2022-03-20 22:39:14 +0530

    Added Milestone 1

---
+++ Milestone 1/4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+       int x;
+       cin>>x;
+       if(x>0){
+           cout<<"Number is positive";
+       }
+       else if (x==0) {
+           cout<<"Number is zero";
+       }
+       else{
+           cout<<"Number is negative";
+       }
+	return 0;
+}
---
+++ Milestone 1/5.cpp
@@ -0,0 +1,15 @@
+
+//Write a program to find multiplication of two numbers?
+
+#include <iostream>
+using namespace std;
+
+int main() {
+       int x,y;
+       cout<<"Enter two numbers :"<<endl;
+       cin>>x>>y;
+       cout<<"Product of these two numbers :";
+       cout<<x*y;
+      
+	return 0;
+}
